ddd1fb7f43e1121aa2b024c35a69aa476dba3b1f
TomKhan63 <tkhan314@ucla.edu>
2023-05-19 16:49:56 -0700

    Changed match statements to if stateents

--- scheduler.py
+++ scheduler.py
@@ -8,24 +8,19 @@ class Scheduler():
     def run(self, quantity, daily_dose, name = ""):
         num_days = quantity/daily_dose
         dose_hours = [] # List of Hours of when to take pill each day
-        match (daily_dose):
-            case 1:
-                dose_hours.append(9) # Take pill at 9 AM each day
-            case 2:
-                dose_hours += [9, 21]
-            case 3:
-                dose_hours += [9, 14, 21]
-            case 4:
-                dose_hours += [9, 13, 17, 21]
-            case 5:
-                dose_hours += [5, 9, 13, 17, 21]
-            case default:
-                subprocess.run(["python3", "error_handler.py"])
-                assert(False)
-                # dose_hours = np.arange(6, 21, 15/7)
-                # dose_minutes = [hour%1 for hour in dose_hours]
-                # dose_hours = np.round(dose_hours, 0)
-                # dose_minutes = np.round(dose_minutes, 2)
+        if daily_dose == 1:
+            dose_hours.append(9) # Take pill at 9 AM each day
+        elif daily_dose == 2:
+            dose_hours += [9, 21]
+        elif daily_dose == 3:
+            dose_hours += [9, 14, 21]
+        elif daily_dose == 4:
+            dose_hours += [9, 13, 17, 21]
+        elif daily_dose == 5:
+            dose_hours += [5, 9, 13, 17, 21]
+        else :
+            subprocess.run(["python3", "error_handler.py"])
+            assert(False)
         local_time = time.localtime(time.time())
         hour = local_time.tm_hour
         # maybe needed depending on how we want to handle pills being put in the machine in the middle of the day
@@ -48,7 +43,7 @@ class Scheduler():
         #print("Crontab Information:\n")
         for job in cron:
             print(job) 
-        #cron.write()
+        cron.write()
 
     def add_counter(self, name, quantity):        
         with open("Counter.json", "r") as file:
